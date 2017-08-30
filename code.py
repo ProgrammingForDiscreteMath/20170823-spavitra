e40f065119c8889bc48216636dfc7d4477d9c7fb
spavitra <31251474+spavitra@users.noreply.github.com>
2017-08-30 23:29:40 +0530

    Add files via upload

--- code.py
+++ code.py
@@ -18,7 +18,13 @@ def entries_less_than_ten(L):
     Returns:
         A sublist of L consisting of those entries which are less than 10.
     """
-    return #Add your code here
+    K=[]
+    for i in L:
+        if i<10:
+            K.append(i)
+        else:
+            pass
+    return K
 
 #Test
 #print entries_less_than_ten([2, 13, 4, 66, -5]) == [2, 4, 6, -5]
@@ -35,7 +41,13 @@ def number_of_negatives(L):
     Returns:
         number of entries of L which are negative
     """
-    pass ##YOUR CODE REPLACES THIS
+    K=0
+    for i in L:
+        if i<0:
+            K+=1
+        else:
+            pass
+    return K
 
 # TEST
 #print number_of_negatives([2, -1, 3, 0, -1, 0, -45, 21]) == 3
@@ -53,7 +65,16 @@ def common_elements(L1, L2):
         A list whose elements are the common elements of ``L1`` and
         ``L2`` WITHOUT DUPLICATES.
     """
-    pass # your code goes here
+    K=[]
+    for i in L1:
+        if i not in K:
+            for j in L2:
+                if i == j:
+                    K.append(i)
+                    break
+                else:
+                    pass
+    return K
 
 #TEST
 #common_elements([1, 2, 1, 4, "bio", 6, 1], [4, 4, 2, 1, 3, 5]) == [1, 2, 4]
@@ -66,7 +87,14 @@ def fibonacci_generator():
     The Fibonacci sequence 1, 1, 2, 3, 5, 8, 13, 21,...
     is defined by a1=1, a2=1, and an = a(n-1) + a(n-2).
     """
-    pass # Hint: use the ``yield`` command.
+    a1=1
+    a2=1
+    
+    while True:
+	    yield a1	    
+	    t=a1
+	    a1=a2
+	    a2=t+a1
 
 #TEST
 #f = fibonacci()
@@ -77,7 +105,18 @@ def largest_fibonacci_before(n):
     """
     Return the largest Fibonacci number less than ``n``.
     """
-    pass #Your code goes here.
+    f=fibonacci_generator()
+    l=0
+    while (True):
+        l2=l
+        l=f.next()
+        
+        if( l>n-1):
+            break
+    return l2
+        
+    
+        
 
 #TEST
 #largest-fibonacci_before(55) == 34
@@ -89,7 +128,12 @@ def catalan_generator():
 
     For the definition of the Catalan number sequence see `OEIS <https://www.oeis.org/A000108>`.
     """
-    pass #Your code goes here.
+    
+    x,y=1,1
+    while True:
+        yield(x)
+        x=(factorial(2*y))/(factorial(y)*(factorial(y+1)))
+        y+=1
 
 #TEST
 #c = catalan_generator()
@@ -101,3 +145,16 @@ def catalan_generator():
 ### CREATE YOUR OWN FUNCTION. Make sure it has a nice docstring.
 # See https://www.python.org/dev/peps/pep-0257/
 # for basic tips on docstrings.
+
+def factorial(n):
+    """
+    find the factorial of a given number n
+    defined by n*factorial(n-1)
+
+    
+    """   
+    if n==1 or n==0:
+        return 1
+    else:
+        fac = n*factorial(n-1)
+    return fac
